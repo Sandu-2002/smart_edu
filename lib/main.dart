c994c0dea22ab6781c68450d283b1dc1363fbd70
Sandu-2002 <sanjayapraveen2@gmail.com>
2026-04-01 00:00:28 +0530

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,6 @@
 import 'package:flutter/material.dart';
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const SmartEduApp());
 }
