0ec6aa40db0687d75da419a3c7b1c0220c51c61c
Sandu-2002 <sanjayapraveen2@gmail.com>
2026-03-31 23:38:05 +0530

    fix main

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,15 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => AuthProvider)),
-        ChangeNotifierProvider(create: (_) => RoleProvider)),
-      ],
-      child: const SmartEduApp(),
-      ),
-  ):
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
 }
