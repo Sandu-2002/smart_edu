69375b9af3444bbdf600413f21df58af85e49e40
Sandu-2002 <sanjayapraveen2@gmail.com>
2026-04-18 23:24:20 +0530

    fire bas connect

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:smart_edu/app/app.dart';
+import 'app.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const SmartEduApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
 }
